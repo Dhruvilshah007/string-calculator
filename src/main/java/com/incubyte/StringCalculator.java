9be8ba1e8db09d5c4de93ea21b465e6a53b1f896
Dhruvilshah007 <sdhruvil72@gmail.com>
2024-07-03 21:46:44 +0530

    FEAT: Empty string and single number in string

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -1,4 +1,13 @@
 package com.incubyte;
 
 public class StringCalculator {
+
+    public int add(String numbers) {
+        if (numbers.isEmpty()) return 0;
+
+        return Integer.parseInt(numbers);
+    }
+
+
+
 }
--- src/test/java/com/incubyte/StringCalculatorShould.java
+++ src/test/java/com/incubyte/StringCalculatorShould.java
@@ -1,4 +1,27 @@
 package com.incubyte;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorShould {
+
+    @Test
+    @DisplayName("Empty string should return 0")
+    void empty_string_should_return_0() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(0, stringCalculator.add(""));
+    }
+
+    @Test
+    @DisplayName("Single number should return number as int")
+    void string_with_single_number_should_return_number_as_int() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(1, stringCalculator.add("1"));
+    }
+
+
+
+
 }
